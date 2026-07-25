045d2c3561f2dd6dbd8ce427ac70e421fbdff565
gopanmodak <gopanhridoy@gmail.com>
2026-07-25 22:06:31 +0600

    try post methode

--- index.js
+++ index.js
@@ -1,69 +1,50 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
-app.use(express.json())
 app.use(cors());
-
-const port = process.env.PORT || 8000;
-
-
-app.get('/', (req, res) => {
-  res.send('This is my server running')
-})
+app.use(express.json());
+const port = process.env.PORT || 9000;
 
 const user = [
   {
     "id": 1,
     "name": "Rahim Ahmed",
-    "email": "rahim@gmail.com",
+    "email": "rahim21@gmail.com",
     "town": "Dhaka"
   },
   {
     "id": 2,
     "name": "Karim Hasan",
-    "email": "karim@gmail.com",
+    "email": "karim2333@gmail.com",
     "town": "Chattogram"
   },
   {
     "id": 3,
     "name": "Sadia Islam",
-    "email": "sadia@gmail.com",
+    "email": "sadiaislam@gmail.com",
     "town": "Rajshahi"
   },
-  {
-    "id": 4,
-    "name": "Nusrat Jahan",
-    "email": "nusrat@gmail.com",
-    "town": "Sylhet"
-  },
-  {
-    "id": 5,
-    "name": "Tanvir Hossain",
-    "email": "tanvir@gmail.com",
-    "town": "Khulna"
-  },
-  {
-    "id": 6,
-    "name": "Mim Akter",
-    "email": "mim@gmail.com",
-    "town": "Barishal"
-  }
 ]
 
-app.get('/user', (req, res) => {
-  res.send(user)
-
+app.get('/', (req,res)=> {
+ res.send('This is from your server side')
 })
 
-app.post('/user', (req, res) => {
-  console.log('User hit this api from client side', req.body)
+
+app.get('/users', (req,res) => {
+  res.send(user)
+})
+
+app.post('/users', (req, res)=> {
+  console.log('User API Hit' , req.body)
 
   const newUser = req.body;
-  newUser.id = user.length + 1;
-  user.push(newUser)
-  res.send(user)
+    newUser.id = user.length + 1
+    user.push(newUser)
+    res.send(user) 
 })
 
-app.listen(port,() => {
-  console.log(`The server is running port: ${port}`)
+app.listen(port, () => {
+  console.log(`Your server is runing on port ${port}`)
 })
