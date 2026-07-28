4dff995d118201cab61c5c8cc9fdf39c0406db77
gopanmodak <gopanhridoy@gmail.com>
2026-07-28 22:44:54 +0600

    put methode

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
+//database password
+//ML0QF9zaJnOE2wym
+
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const app = express();
-const cors = require('cors');
+
+const cors = require('cors')
 app.use(cors());
-app.use(express.json())
-
-
-
-
-//mongodb using
-
-
-
+app.use(express.json());
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://gopanhridoy_db_user:ML0QF9zaJnOE2wym@cluster0.xoivzyj.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,68 +22,78 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-
-    const database = client.db('userDB')
-    const userCollection = database.collection('newUser')
-
-
     await client.connect();
 
 
-    app.get('/list', async (req, res)=> {
+    const database =client.db('learning-backend')
+    const userCollection = database.collection('user')
+        
+    //get methode
+    app.get('/users', async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result)
     })
 
-    app.post('/userlist',async (req, res)=> {
-      const userList = req.body; 
-      console.log('user add database',userList)
+    //get methode find one
 
-      const result = await userCollection.insertOne(userList);
-      res.send(result)
-    })
-
-
-    app.delete('/list/:id', async (req, res)=>{
-      const id = req.params.id;
-      console.log('delete this id: ',id)
+    app.get('/users/:id', async (req,res)=>{
+      const id =req.params.id;
       const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
+      const result = await userCollection.findOne(query);
       res.send(result)
     })
+
+       //post methode
+       app.post('/users', async (req, res) => {
+        console.log('User try to post : ',req.body)
+
+        const users = req.body;
+        const result = await userCollection.insertOne(users);
+        res.send(result)
+       })
+
+       //delete methode
+
+       app.delete('/users/:id',async(req,res)=>{
+          const id =req.params.id;
+          const query ={_id: new ObjectId(id)}
+          const result = await userCollection.deleteOne(query)
+          res.send(result)
+       })
+
+
+       //put methode
+       app.put('/users/:id',async (req, res) =>{
+        const id =req.params.id;
+        const updateUser = req.body;
+        console.log(updateUser)
+
+        const filter ={_id: new ObjectId(id)}
+        const options = { upsert: true };
+  const userUpdate ={
+    $set:{
+      name :updateUser.name,
+    email:updateUser.email,
+    town:updateUser.town
+    }
+    
+  }
+  const result = await userCollection.updateOne(filter, userUpdate, options);
+  res.send(result)
+       })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-   /*  await client.close(); */
+    /* await client.close(); */
   }
 }
 run().catch(console.dir);
 
 
-/* const products = require('./data/products.json')
-
-app.get('/products' , (req, res) => {
-  res.send(products)
-})
-
-app.post('/products', (req, res) => {
-
-  console.log('user hit the post api' ,req.body)
-
-  const newProducts = req.body;
-   newProducts.id = products.length + 1;
-   products.push(newProducts);
-   res.send(products)
-}) */
-
 
 
 module.exports = app;
-
-
-
-//gopanhridoy_db_user
-
-//ML0QF9zaJnOE2wym
